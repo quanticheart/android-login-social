c16764b4ef24eacb1f7987fff59ef6b2cc76042e
jonny <dev.jonny255d@gmail.com>
2020-03-29 01:07:10 -0300

    added button apple login

--- loginsocial/src/main/java/com/example/loginsocial/ui/AppleButton.kt
+++ loginsocial/src/main/java/com/example/loginsocial/ui/AppleButton.kt
@@ -31,7 +31,7 @@
  *  *        |/_/         \===/
  *  *                       =
  *  *
- *  * Copyright(c) Developed by John Alves at 2020/3/28 at 6:30:6 for quantic heart studios
+ *  * Copyright(c) Developed by John Alves at 2020/3/29 at 0:57:6 for quantic heart studios
  *
  */
 
@@ -43,38 +43,19 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
-import androidx.fragment.app.Fragment
 import com.example.loginsocial.R
-import com.example.loginsocial.google.startGoogleLogin
-import com.example.loginsocial.google.startGoogleLoginFragment
-import com.example.loginsocial.toolbox.entity.UserSocialData
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import kotlinx.android.synthetic.main.layout_google_button.view.*
 
-class GoogleButton @JvmOverloads constructor(
+class AppleButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     private var button: View =
-        LayoutInflater.from(context).inflate(R.layout.layout_google_button, this, false)
+        LayoutInflater.from(context).inflate(R.layout.layout_apple_button, this, false)
 
     init {
         addView(button)
     }
 
     fun init(activity: Activity) {
-        button.btnLoginGoogle.startGoogleLogin(activity)
-    }
-
-    fun init(fragment: Fragment) {
-        button.btnLoginGoogle.startGoogleLoginFragment(fragment)
-    }
-
-    fun init(activity: Activity, callback: (UserSocialData?, GoogleSignInAccount?) -> Unit) {
-        button.btnLoginGoogle.startGoogleLogin(activity, callback)
-    }
-
-    fun init(fragment: Fragment, callback: (UserSocialData?, GoogleSignInAccount?) -> Unit) {
-        button.btnLoginGoogle.startGoogleLoginFragment(fragment, callback)
     }
 }
